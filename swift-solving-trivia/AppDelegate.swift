1d7d19a97b715d85ace89e90ab8d82f360278df2
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-09-12 13:00:14 -0400

    Done

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -12,11 +12,31 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    /*
-     
-     write your function here
-     
-     */
+  
+    
+    func isCharacterMatching(state:String, capital:String) ->Bool {
+        for char in state.lowercaseString.characters {
+            for charr in capital.lowercaseString.characters {
+                if char == charr {
+                    return false
+                }
+                
+            }
+        }
+        return true
+    }
 
+
+
+
+    func solveTrivia(dictionary:[String:String]) ->String {
+    var result:String = ""
+    for (state, capital) in dictionary {
+        if isCharacterMatching(state, capital: capital) {
+            result = "\(state)"
+            return result
+        }
 }
-
+          return result
+}
+}
